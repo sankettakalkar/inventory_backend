80e4f6b3aef54af07f54d78dad45993fdeebcc35
Sanket Takalkar <takalkar.sanket@gmail.com>
2026-01-29 11:53:44 +0530

    Updated Vercel cors

--- app/main.py
+++ app/main.py
@@ -11,7 +11,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:5173",
-        "https://inventorybackend-production-0b9e.up.railway.app"
+        "https://inventory-frontend-gurvz847r-sankettakalkars-projects.vercel.app"
     ],
     allow_credentials=True,
     allow_methods=["*"],
