c42fdc949f5bfaccd3eee029862ab4685f81c6e0
Sanket Takalkar <takalkar.sanket@gmail.com>
2026-01-29 14:57:17 +0530

    Minor fix

--- app/main.py
+++ app/main.py
@@ -11,7 +11,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:5173",
-        "https://inventory-frontend-sigma-mocha.vercel.app/"
+        "https://inventory-frontend-sigma-mocha.vercel.app"
     ],
     allow_credentials=False,
     allow_methods=["*"],
