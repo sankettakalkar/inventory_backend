e3649b1902331ba3ad5eac01a7670f90e045f6f8
Sanket Takalkar <takalkar.sanket@gmail.com>
2026-01-29 11:30:52 +0530

    Updated CORS

--- app/main.py
+++ app/main.py
@@ -10,8 +10,8 @@ app = FastAPI(title="Inventory Management API")
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "http://localhost:3000",
-        "http://localhost:5173"
+        "http://localhost:5173",
+        "https://inventorybackend-production-0b9e.up.railway.app"
     ],
     allow_credentials=True,
     allow_methods=["*"],
